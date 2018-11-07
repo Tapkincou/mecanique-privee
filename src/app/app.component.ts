005ff7824d4f3da189d27f7b22dcfcb7ef44219f
Tapkincou <m.barreau.alexis@gmail.com>
2018-11-07 22:37:21 +0100

    [ADD] 'VIEW' constantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'mecanique-privee';
+  private currentview;
+  private BILL_VIEW = 'app-bill-view';
+  private PRODUCT_VIEW = 'app-product-view';
+  private CUSTOMER_VIEW = 'app-customer-view';
+  private ESTIMATE_VIEW = 'app-estimate-view';
+  constructor () {
+    this.currentview = this.BILL_VIEW;
+  }
+
+
 }

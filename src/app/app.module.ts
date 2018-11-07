81cacc59f0eea67e722b7ae236f1c573f502e338
Tapkincou <m.barreau.alexis@gmail.com>
2018-11-07 22:16:35 +0100

    [ADD] sidenav & buttons
    bottom-right corner button + more options on menubar + new sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatMenuModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatMenuModule, MatButtonModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BillViewComponent } from './bill-view/bill-view.component';
+import { CustomerViewComponent } from './customer-view/customer-view.component';
+import { EstimateViewComponent } from './estimate-view/estimate-view.component';
+import { ProductViewComponent } from './product-view/product-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillViewComponent,
+    CustomerViewComponent,
+    EstimateViewComponent,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule,
@@ -15,7 +23,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
